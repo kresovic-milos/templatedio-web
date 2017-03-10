0ff5810679968c3a018e15e3f577bec064ea310f
kresovic-milos <kresovic.milos@gmail.com>
2017-03-10 01:53:30 +0100

    dropzone added

--- src/routes/wizard/GetStarted.jsx
+++ src/routes/wizard/GetStarted.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
 
 class GetStarted extends Component {
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    console.log('Accepted files: ', acceptedFiles)
+    console.log('Rejected files: ', rejectedFiles)
+  }
+
   render() {
-    return (   
-            <h1>GetStarted</h1>     
+    return (
+
+      <div>
+        <h1>Get Started</h1>
+
+        <Dropzone onDrop={this.onDrop}>
+          <p>Drop your template here, or click to select file to upload.</p>
+        </Dropzone>
+      </div>
     )
   }
 }
